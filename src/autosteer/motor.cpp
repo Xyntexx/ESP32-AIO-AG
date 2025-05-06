74ddd1639953cd7ca9206be594b2e7f23e307f5b
MarkusNuuja <markus.nuuja@simucube.com>
2025-05-06 13:44:24 +0300

    add motor reverse feature

--- src/autosteer/motor.cpp
+++ src/autosteer/motor.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "motor.h"
-#include "utils/log.h"
+#include "settings/settings.h"
 
 // Track initialization state
 static bool initialized = false;
@@ -11,37 +11,39 @@ static bool initialized = false;
 static uint8_t currentPWM = 0;
 
 namespace motor {
-    // Static interface pointer
-    static MotorInterface* hw_interface = nullptr;
+// Static interface pointer
+static MotorInterface *hw_interface = nullptr;
 
-    bool init(const MotorInterface& hw) {
-        hw_interface = const_cast<MotorInterface*>(&hw);
-        return true;
+bool init(const MotorInterface &hw) {
+    hw_interface = const_cast<MotorInterface *>(&hw);
+    return true;
+}
+
+/**
+ * Get current PWM value
+ */
+uint8_t getCurrentPWM() {
+    if (hw_interface && hw_interface->getPwm) {
+        return hw_interface->getPwm();
     }
-    /**
-     * Get current PWM value
-     */
-    uint8_t getCurrentPWM() {
-        if (hw_interface && hw_interface->getPwm) {
-            return hw_interface->getPwm();
-        }
-        return currentPWM;
+    return currentPWM;
+}
+
+// Global functions used by autosteer
+void driveMotor(uint8_t pwm, bool reversed) {
+
+    if (Set.invertSteer) {
+        reversed = !reversed;
     }
 
-    // Global functions used by autosteer
-    void driveMotor(uint8_t pwm, bool reversed) {
-        if (hw_interface && hw_interface->drive) {
-            hw_interface->drive(pwm, reversed);
-        } else {
-            error("Motor hardware interface not initialized");
-        }
+    if (hw_interface && hw_interface->drive) {
+        hw_interface->drive(pwm, reversed);
     }
+}
 
-    void stopMotor() {
-        if (hw_interface && hw_interface->stop) {
-            hw_interface->stop();
-        } else {
-            error("Motor hardware interface not initialized");
-        }
+void stopMotor() {
+    if (hw_interface && hw_interface->stop) {
+        hw_interface->stop();
     }
+}
 }
