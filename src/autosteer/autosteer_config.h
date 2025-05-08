7481c6aa6749b33bbb900c4359f0cb62b1c9339c
Xyntexx <21282105+Xyntexx@users.noreply.github.com>
2025-05-08 09:36:25 +0300

    fix watchdog timeout

--- src/autosteer/autosteer_config.h
+++ src/autosteer/autosteer_config.h
@@ -6,7 +6,7 @@
 #define AUTOSTEER_CONFIG_H
 
 #define LOW_HIGH_DEGREES 3.0
-#define WATCHDOG_TIMEOUT 100 // Watchdog timeout in milliseconds
+#define WATCHDOG_TIMEOUT 200 // Watchdog timeout in milliseconds
 
 
 #endif //AUTOSTEER_CONFIG_H
